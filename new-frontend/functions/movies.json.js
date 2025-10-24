a668c3ddec22d43ab2e2783692732ad724e98d1d
Jordan Crawford <jordan@crawford.kiwi>
2025-10-24 18:08:55 +1300

    WIP

--- new-frontend/functions/movies.json.js
+++ new-frontend/functions/movies.json.js
@@ -1,59 +1,63 @@
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer xxx'
-  }
-};
-
-const today = new Date();
-const tomorrow = new Date();
-tomorrow.setDate(today.getDate() + 1);
-const threeMonthsAgo = new Date();
-threeMonthsAgo.setDate(today.getDate() - 90);
-
-function dateToIsoDateOnly(date) {
-  return date.toISOString().substring(0, 10);
+export function onRequest(context) {
+  return new Response("Hello, world!")
 }
 
-const todayString = dateToIsoDateOnly(today);
-const tomorrowString = dateToIsoDateOnly(tomorrow);
-const threeMonthsAgoString = dateToIsoDateOnly(threeMonthsAgo);
-console.log(todayString)
-console.log(tomorrowString)
-console.log(threeMonthsAgoString)
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer '
+//   }
+// };
 
-const nowShowingParams = new URLSearchParams({
-  'sort_by': 'popularity.desc',
-  'include_adult': false,
-  'primary_release_date.gte': threeMonthsAgoString,
-  'primary_release_date.lte': todayString,
-  'with_release_type': '2|3',
-  'with_original_language': 'en'
-});
+// const today = new Date();
+// const tomorrow = new Date();
+// tomorrow.setDate(today.getDate() + 1);
+// const threeMonthsAgo = new Date();
+// threeMonthsAgo.setDate(today.getDate() - 90);
 
-const upcomingParams = new URLSearchParams({
-  'sort_by': 'popularity.desc',
-  'include_adult': false,
-  'primary_release_date.gte': tomorrowString,
-  'with_release_type': '2|3',
-  'with_original_language': 'en'
-});
+// function dateToIsoDateOnly(date) {
+//   return date.toISOString().substring(0, 10);
+// }
 
-async function fetchMoviesAndTrailers(params) {
-  let movieResult = await fetch(`https://api.themoviedb.org/3/discover/movie?${params}`, options)
-    .then(response => response.json());
-  // TODO: Handle not success
-  let movieIds = movieResult.results.map((movie) => movie.id);
+// const todayString = dateToIsoDateOnly(today);
+// const tomorrowString = dateToIsoDateOnly(tomorrow);
+// const threeMonthsAgoString = dateToIsoDateOnly(threeMonthsAgo);
+// console.log(todayString)
+// console.log(tomorrowString)
+// console.log(threeMonthsAgoString)
 
-  const fetchTrailerPromises = movieIds.map(movieId => {
-    return fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, options)
-      .then(response => response.json())
-  });
-  const trailers = await Promise.all(fetchTrailerPromises)
+// const nowShowingParams = new URLSearchParams({
+//   'sort_by': 'popularity.desc',
+//   'include_adult': false,
+//   'primary_release_date.gte': threeMonthsAgoString,
+//   'primary_release_date.lte': todayString,
+//   'with_release_type': '2|3',
+//   'with_original_language': 'en'
+// });
 
-  console.log(trailers);
-}
+// const upcomingParams = new URLSearchParams({
+//   'sort_by': 'popularity.desc',
+//   'include_adult': false,
+//   'primary_release_date.gte': tomorrowString,
+//   'with_release_type': '2|3',
+//   'with_original_language': 'en'
+// });
 
-fetchMoviesAndTrailers(nowShowingParams);
-fetchMoviesAndTrailers(upcomingParams)
+// async function fetchMoviesAndTrailers(params) {
+//   let movieResult = await fetch(`https://api.themoviedb.org/3/discover/movie?${params}`, options)
+//     .then(response => response.json());
+//   // TODO: Handle not success
+//   let movieIds = movieResult.results.map((movie) => movie.id);
+
+//   const fetchTrailerPromises = movieIds.map(movieId => {
+//     return fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, options)
+//       .then(response => response.json())
+//   });
+//   const trailers = await Promise.all(fetchTrailerPromises)
+
+//   console.log(trailers);
+// }
+
+// fetchMoviesAndTrailers(nowShowingParams);
+// // fetchMoviesAndTrailers(upcomingParams)
