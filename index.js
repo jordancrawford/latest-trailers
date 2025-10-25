a12935a1576d50aefde161e4a15d63ca2a816576
Jordan Crawford <jordan@crawford.kiwi>
2025-10-26 00:12:25 +1300

    Fetch data if no cache data is present

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import { showingKey, upcomingKey, fetchShowingTrailers, fetchUpcomingTrailers } from './fetcher.js';
 
+const fetchAndCacheTrailers = async (key, env, refresh) => {
+  let cachedData = refresh ? null : await env.trailerCache.get(key);
+
+  if (!cachedData) {
+    const tmdbToken = env.TMDB_TOKEN;
+    const trailers = key == showingKey ? await fetchShowingTrailers(tmdbToken) : await fetchUpcomingTrailers(tmdbToken);
+    await env.trailerCache.put(key, JSON.stringify(trailers));
+    console.info(`Saved ${key}} trailers`);
+
+    return trailers;
+  } else {
+    return JSON.parse(cachedData);
+  }
+};
+
 export default {
-  async scheduled(controller, env, ctx) {
+  async scheduled(_, env, ctx) {
     console.info("Scheduled fetch triggered");
 
     ctx.waitUntil((async () => {
-      const tmdbToken = env.TMDB_TOKEN;
-
-      const showingTrailers = await fetchShowingTrailers(tmdbToken);
-      await env.trailerCache.put(showingKey, JSON.stringify(showingTrailers));
-      console.info("Saved showing trailers");
-
-      const upcomingTrailers = await fetchUpcomingTrailers(tmdbToken);
-      await env.trailerCache.put(upcomingKey, JSON.stringify(upcomingTrailers));
-      console.info("Saved upcoming trailers");
+      await fetchAndCacheTrailers(showingKey, env, true)
+      await fetchAndCacheTrailers(upcomingKey, env, true)
 
       if (env.FETCH_SUCCESS_NOTIFICATION_URL) {
         await fetch(env.FETCH_SUCCESS_NOTIFICATION_URL);
@@ -27,12 +35,8 @@ export default {
     const url = new URL(request.url);
     const path = url.pathname;
 
-    if (path === "/showing.json")   {
-      const trailers = JSON.parse(await env.trailerCache.get(showingKey));
-      return Response.json(trailers);
-    } else {
-      const trailers = JSON.parse(await env.trailerCache.get(upcomingKey));
-      return Response.json(trailers);
-    }
-  },
-}
+    const key = path === "/showing.json" ? showingKey : upcomingKey;
+    const trailers = await fetchAndCacheTrailers(key, env, false);
+    return Response.json(trailers);
+  }
+};
